23b5e7183b8aad251c737ec3946a3eee9c43de76
JoshMC <commission.josh@gmail.com>
2025-06-07 08:12:38 +0800

    getimg

--- netlify/functions/getImages.js
+++ netlify/functions/getImages.js
@@ -2,20 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async () => {
-  try {
-    const imgDir = path.join(__dirname, '../../img');
-    const files = fs.readdirSync(imgDir).filter(file =>
-      /\.(png|jpe?g|gif|webp)$/i.test(file)
-    );
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(files),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to read image folder' }),
-    };
-  }
+    const dir = path.join(__dirname, '../../img');
+    try {
+        const files = fs.readdirSync(dir);
+        const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif|webp)$/i.test(file));
+        return {
+            statusCode: 200,
+            body: JSON.stringify(imageFiles)
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to read images directory' })
+        };
+    }
 };
